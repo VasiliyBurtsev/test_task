693600bae34f7a90de1d71313edd0f24cb0dc151
VasiliyBurtsev <143483566+VasiliyBurtsev@users.noreply.github.com>
2023-09-05 16:30:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@ int main(int, char**){
     setlocale(LC_ALL,"");
     //system("CLS");
     //say_hello();
-    string js_str = readList(REST_API,"p10", "i586","export/branch_binary_packages");
+    string REST_API = "http://rdb.altlinux.org";
+         string method = "export/branch_binary_packages";
+         string branch = "p10";
+         string arch = "i586";
+		 string js_str = readJSONAPI(REST_API,branch,arch,method);
        //system("pause");
     return 0;
 }
