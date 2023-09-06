2d4d6d0a8d38c15eabdb30058d6a671cf3a89981
VasiliyBurtsev <143483566+VasiliyBurtsev@users.noreply.github.com>
2023-09-06 16:39:57 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 
 int main(int, char**){
-    setlocale(LC_ALL,"");
-    
-    string REST_API = "http://rdb.altlinux.org";
-         string method = "export/branch_binary_packages";
-         string branch = "p10";
-         string arch = "i586";
-		 string js_str = readJSONAPI(REST_API,branch,arch,method);
+    setlocale(LC_ALL,"");    
+    string method = "export/branch_binary_packages";
+    string branch1 = "";
+    srting branch2 = "";
+    string arch;
+    cout << "Enter the first branch ";
+    cin >> branch1;
+    cout << "Enter the second branch ";
+    cin >> branch2;
+   string js_str1 = readJSONAPI(REST_API,branch1,arch,method);
+   string js_str2 = readJSONAPI(REST_API,branch2,arch,method);
        //system("pause");
     return 0;
 }
